5b193f9a33241ade810eab3ed7a22a1c29d29357
Alex Stanbury <alexstanbury@googlemail.com>
2022-06-17 17:05:57 +0100

    fix: add permission check

--- Camera.js
+++ Camera.js
@@ -1,5 +1,5 @@
-import {StyleSheet, View} from 'react-native';
-import React, {useState} from 'react';
+import {StyleSheet, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import {
   useCameraDevices,
   Camera as RNVC,
@@ -7,9 +7,31 @@ import {
 } from 'react-native-vision-camera';
 import {runOnJS} from 'react-native-reanimated';
 
+const useCameraPermission = () => {
+  const [hasPermission, setHasPermission] = useState(false);
+
+  useEffect(() => {
+    async function checkPermission() {
+      const cameraPermission = await RNVC.getCameraPermissionStatus();
+      console.log({cameraPermission});
+      if (cameraPermission === 'authorized') {
+        setHasPermission(true);
+      } else {
+        console.log('request', {cameraPermission});
+        const status = await RNVC.requestCameraPermission();
+        setHasPermission(status === 'authorized');
+      }
+    }
+    checkPermission();
+  }, []);
+
+  return hasPermission;
+};
+
 export default function Camera() {
   const devices = useCameraDevices();
   const device = devices.back;
+  const hasPermission = useCameraPermission();
   const [data, setData] = useState(0);
 
   function update() {
@@ -21,10 +43,8 @@ export default function Camera() {
     // runOnJS(update)();
   }, []);
 
-  if (!device) {
-    return (
-      <View style={{width: 100, height: 100, backgroundColor: 'red'}}></View>
-    );
+  if (!device || !hasPermission) {
+    return <Text>No permission</Text>;
   }
 
   return (
